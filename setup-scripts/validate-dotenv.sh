4f490de48484922fd261318a7d6b41fbf4077ca2
sunakan <sunakan03@gmail.com>
2024-12-26 13:08:50 +0900

    kaizen-scriptとsetup-scriptに `-o pipefail` を追加

--- setup-scripts/validate-dotenv.sh
+++ setup-scripts/validate-dotenv.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-set -eu
+set -euo pipefail
 #set -x
 # -e: エラーが発生した時点でスクリプトを終了
 # -u: 未定義の変数を使用した場合にエラーを発生
 # -x: スクリプトの実行内容を表示(debugで利用)
+# -o pipefail: パイプライン内のエラーを検出
 
 #
 # 通知
